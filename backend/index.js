cd9d882dfd1bfd8bd65e808d25eb43a4da94e714
Usman <usmanrehmani126@gmail.com>
2024-11-19 02:30:54 -0800

    Add social login provider with github and setup navbar

---
+++ backend/index.js
@@ -0,0 +1,1 @@
+console.log("Currently backend is not availble")
---
+++ frontend/app/components/Navbar.tsx
@@ -0,0 +1,48 @@
+import { auth, signIn, signOut } from "@/auth";
+import Image from "next/image";
+import Link from "next/link";
+
+const Navbar = async () => {
+  const session = await auth();
+  return (
+    <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
+      <nav className="flex items-center justify-between">
+        <Link href="/">
+          <Image src="/logo.png" alt="logo" width={140} height={30} />
+        </Link>
+        <div className="flex items-center gap-5 text-black">
+          {session && session.user ? (
+            <>
+              <Link href="/startup/create">
+                <span>Create</span>
+              </Link>
+              <form
+                action={async () => {
+                  "use server";
+                  await signOut();
+                }}
+              >
+                <span>Logout</span>
+              </form>
+              <Link href={`/user/${session?.id}`}>
+                <span>{session.user.name}</span>
+              </Link>
+            </>
+          ) : (
+            <form
+              action={async () => {
+                "use server";
+
+                await signIn("github");
+              }}
+            >
+              <button type="submit">Login</button>
+            </form>
+          )}
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
